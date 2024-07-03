085767b7917ed3dbdb44310708e32a66c1384f35
lgandarillas <lgandaf@gmail.com>
2024-07-03 16:39:11 +0200

    Add: print_error.c

--- extra/print_error.c
+++ extra/print_error.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   extra.h                                            :+:      :+:    :+:   */
+/*   ft_print_error.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
+/*   By: lgandari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/06/07 12:59:00 by lgandari          #+#    #+#             */
-/*   Updated: 2024/07/03 16:36:37 by lgandari         ###   ########.fr       */
+/*   Created: 2024/07/03 16:34:27 by lgandari          #+#    #+#             */
+/*   Updated: 2024/07/03 16:35:12 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef EXTRA_H
-# define EXTRA_H
+#include "../inc/extra.h"
 
-int	ft_strcmp(const char *s1, const char *s2);
-int	print_error(char *msg, int exit_code);
-
-#endif
+int	print_error(char *msg, int exit_code)
+{
+	ft_putstr_fd(msg, STDERR_FILENO);
+	exit(exit_code);
+}
