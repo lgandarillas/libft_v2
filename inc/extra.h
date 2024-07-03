e3b4228c6399dc69784146a2a16b5339b0f55ab8
lgandarillas <lgandaf@gmail.com>
2024-07-03 17:04:14 +0200

    Update header extra.h

--- inc/extra.h
+++ inc/extra.h
@@ -6,12 +6,17 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 12:59:00 by lgandari          #+#    #+#             */
-/*   Updated: 2024/07/03 16:36:37 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/07/03 16:59:51 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXTRA_H
 # define EXTRA_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# include "../inc/libft.h"
 
 int	ft_strcmp(const char *s1, const char *s2);
 int	print_error(char *msg, int exit_code);
