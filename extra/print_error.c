a5bdb9d4ca5c41f02eb5910f012bdf3a5d936b6d
lgandarillas <lgandaf@gmail.com>
2024-07-04 18:12:52 +0200

    Update: print_error()

--- extra/print_error.c
+++ extra/print_error.c
@@ -1,19 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_error.c                                   :+:      :+:    :+:   */
+/*   print_error.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lgandari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 16:34:27 by lgandari          #+#    #+#             */
-/*   Updated: 2024/07/03 16:35:12 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/07/04 18:12:00 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/extra.h"
 
-int	print_error(char *msg, int exit_code)
+int	print_error(char *msg)
 {
 	ft_putstr_fd(msg, STDERR_FILENO);
-	exit(exit_code);
+	exit(EXIT_FAILURE);
 }
--- inc/extra.h
+++ inc/extra.h
@@ -6,7 +6,7 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 12:59:00 by lgandari          #+#    #+#             */
-/*   Updated: 2024/07/03 20:19:41 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/07/04 18:12:30 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include "../inc/libft.h"
 
 void	free_matrix(char **matrix);
-int	ft_strcmp(const char *s1, const char *s2);
-int	print_error(char *msg, int exit_code);
+int		ft_strcmp(const char *s1, const char *s2);
+int		print_error(char *msg);
 
 #endif
