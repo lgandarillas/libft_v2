4abe8b7f82d16426e2cdcc94c017d78b4fafa1ec
lgandarillas <lgandaf@gmail.com>
2024-07-18 21:36:38 +0200

    Fix minor error in extra/ft_matrixdup.c

--- extra/ft_matrixdup.c
+++ extra/ft_matrixdup.c
@@ -6,7 +6,7 @@
 /*   By: lgandari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 21:00:24 by lgandari          #+#    #+#             */
-/*   Updated: 2024/07/18 21:21:17 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/07/18 21:35:51 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,10 @@ char	**ft_matrixdup(char **old_matrix)
 {
 	char	**new_matrix;
 	size_t	len;
-	size_t	line;
 	size_t	i;
 
-	line = 2;
 	len = ft_arrlen((void **)old_matrix);
-	new_matrix = ft_calloc(sizeof(char *), len + line);
+	new_matrix = ft_calloc(sizeof(char *), len + 1);
 	if (!new_matrix)
 		return (NULL);
 	i =  0;
