2560c106fb67b8093db06183a26e4ec77ed7fcf4
lgandarillas <lgandaf@gmail.com>
2024-07-18 22:09:19 +0200

    Fix: Norminette errors.

--- extra/ft_matrixdup.c
+++ extra/ft_matrixdup.c
@@ -6,7 +6,7 @@
 /*   By: lgandari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/18 21:00:24 by lgandari          #+#    #+#             */
-/*   Updated: 2024/07/18 21:35:51 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/07/18 22:09:03 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	**ft_matrixdup(char **old_matrix)
 	new_matrix = ft_calloc(sizeof(char *), len + 1);
 	if (!new_matrix)
 		return (NULL);
-	i =  0;
+	i = 0;
 	while (i < len)
 	{
 		new_matrix[i] = ft_strdup(old_matrix[i]);
