d0a58ef25587426118dfdb623a903c1cfa44fb69
lgandarillas <lgandaf@gmail.com>
2024-08-05 22:37:46 +0200

    Protect: NULL parameters in extra/ft_strcmp.c

--- extra/ft_strcmp.c
+++ extra/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 12:52:56 by lgandari          #+#    #+#             */
-/*   Updated: 2024/06/07 12:58:04 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/08/05 22:37:21 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int	ft_strcmp(const char *s1, const char *s2)
 {
 	unsigned int	i;
 
+	if (!s1 || !s2)
+		return (-1);
 	i = 0;
 	while (s1[i] == s2[i] && s1[i] && s2[i])
 		i++;
