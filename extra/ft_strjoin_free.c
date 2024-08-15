651cd1fbf317d0b96dd910daf0d2c288e9982648
lgandarillas <lgandaf@gmail.com>
2024-08-15 15:02:58 +0200

    Update: extra/ft_strjoin_free.c control with NULL params and non allocated arrays.

--- extra/ft_strjoin_free.c
+++ extra/ft_strjoin_free.c
@@ -6,18 +6,22 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 19:28:30 by lgandari          #+#    #+#             */
-/*   Updated: 2024/08/13 19:29:33 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/08/15 15:01:16 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/extra.h"
 
-char	*ft_strjoin_free(char *s1, char *s2)
+char	*ft_strjoin_free(char *s1, char *s2, bool free_s1, bool free_s2)
 {
 	char	*joined_str;
 
 	joined_str = ft_strjoin(s1, s2);
-	free(s1);
-	free(s2);
+	if (!joined_str)
+		return (NULL);
+	if (free_s1 && s1)
+		free(s1);
+	if (free_s2 && s2)
+		free(s2);
 	return (joined_str);
 }
--- inc/extra.h
+++ inc/extra.h
@@ -6,7 +6,7 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/07 12:59:00 by lgandari          #+#    #+#             */
-/*   Updated: 2024/08/13 19:33:33 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/08/15 15:02:06 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ int		print_error(char *msg);
 int		ft_countchar(const char *s, char c);
 bool	ft_strdigit(const char *str);
 char	*ft_strndup(const char *s, size_t n);
-char	*ft_strjoin_free(char *s1, char *s2);
+char	*ft_strjoin_free(char *s1, char *s2, bool free_s1, bool free_s2);
 char	*ft_substr_free(char *s, unsigned int start, size_t len);
 
 #endif
