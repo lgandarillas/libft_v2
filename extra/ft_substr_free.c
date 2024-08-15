a8a05f889ccb8f80288b9ed8e2f734eaf3416675
lgandarillas <lgandaf@gmail.com>
2024-08-15 15:05:11 +0200

    Update: extra/ft_substr_free.c control with NULL params.

--- extra/ft_substr_free.c
+++ extra/ft_substr_free.c
@@ -6,7 +6,7 @@
 /*   By: lgandari <lgandari@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/13 19:32:07 by lgandari          #+#    #+#             */
-/*   Updated: 2024/08/13 19:33:19 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/08/15 15:04:41 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,11 @@ char	*ft_substr_free(char *s, unsigned int start, size_t len)
 {
 	char	*sub_str;
 
+	if (!s)
+		return (NULL);
 	sub_str = ft_substr(s, start, len);
+	if (!sub_str)
+		return (NULL);
 	free(s);
 	return (sub_str);
 }
