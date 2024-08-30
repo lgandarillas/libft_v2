18c3bd9e3f93ebd6fd0eb562cf248a738eee5e48
lgandarillas <lgandaf@gmail.com>
2024-08-30 19:29:46 +0200

    Protect: ft_arrlen()

--- extra/ft_arrlen.c
+++ extra/ft_arrlen.c
@@ -6,7 +6,7 @@
 /*   By: aquinter <aquinter@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 16:37:24 by aquinter          #+#    #+#             */
-/*   Updated: 2024/07/09 18:24:53 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/08/30 19:29:22 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_arrlen(void **arr)
 {
 	size_t	i;
 
+	if (arr == NULL)
+		return (0);
 	i = 0;
 	while (arr[i] != NULL)
 		i++;
