9475b82ceaac3b98099f28cb55cf201323e3c923
lgandarillas <lgandaf@gmail.com>
2024-08-30 19:32:35 +0200

    Protect: free_matrix()

--- extra/free_matrix.c
+++ extra/free_matrix.c
@@ -6,7 +6,7 @@
 /*   By: lgandari <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/03 20:16:15 by lgandari          #+#    #+#             */
-/*   Updated: 2024/07/11 19:55:01 by lgandari         ###   ########.fr       */
+/*   Updated: 2024/08/30 19:32:24 by lgandari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,13 @@ void	free_matrix(char **matrix)
 {
 	int	i;
 
+	if (matrix == NULL)
+		return ;
 	i = 0;
 	while (matrix[i] != NULL)
-		free(matrix[i++]);
+	{
+		free(matrix[i]);
+		i++;
+	}
 	free(matrix);
-	matrix = NULL;
-	return ;
 }
